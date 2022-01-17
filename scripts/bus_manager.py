b9059cb8c0f1423b1fb64d470d9ab506ba5e86ec
CRalliNA <charlie.ralli@nickabbey.co.uk>
2022-01-17 11:31:32 +0000

    added tc_ststus.py

--- scripts/bus_manager.py
+++ scripts/bus_manager.py
@@ -9,7 +9,8 @@ from msg_funcs import *
 
 def callback(data,args):
     pub=args[0]
-    message=args[1]
+    status_pub=args[1]
+    message=args[2]
     my_address=236#make this variable 
     
     #Looking for messages requesting we respond
@@ -18,7 +19,7 @@ def callback(data,args):
         bus_respond(pub,message,data,my_address) #function sends response to request from another ecu on the bus
     elif (data.ID>>16)&0xFF==234 and data.Data==b'\x00\xee\x00':
         address_claim(pub,message) #responds to address claim message
-    
+    	status_pub.pubish('1')
     
     elif (data.ID>>16)&0xFF==203 and (data.ID>>8)&0xFF==236:#Responds to process data messages
         implement_respond(pub,message,data,my_address)
@@ -30,6 +31,8 @@ def listener():
     rospy.init_node('bus_manager', anonymous=True)
     rospy.loginfo('start')
     pub=rospy.Publisher('CAN_to_send', can_frame, queue_size=10)
+    status_pub=rospy.Publisher('tc_status', String, queue_size=10)
+    
     message=can_frame()
     rospy.sleep(0.5)
     #Send address request message
@@ -42,8 +45,10 @@ def listener():
     rospy.sleep(0.1)
     #Calls function to send a message containing our NAME and address
     address_claim(pub,message)
+    status_pub.publish('1')
     
-    rospy.Subscriber('Received_CAN', can_frame, callback,(pub,message))
+    
+    rospy.Subscriber('Received_CAN', can_frame, callback,(pub,status_pub,message))
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
---
+++ scripts/tc_status.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+from telematics.msg import can_frame
+
+
+def callback(data,args):
+
+    if data=='1':       
+        rospy.sleep(6)
+        while not rospy.is_shutdown():
+	        message.ID=0x0CCBFFEC
+	        message.Dest=255
+	        message.Data=b'\xfe\xff\xff\xff\x00\x00\x00\x00'
+	        pub.publish(message) 
+            rospy.sleep(2)
+              
+    
+def talker():
+    pub = rospy.Publisher('CAN_to_send', can_frame, queue_size=10)
+    rospy.init_node('tc_status', anonymous=True)
+
+    message=can_frame()
+    
+
+    rospy.Subscriber('tc_status',String,callback,(pub,message))
+    rospy.spin()
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
